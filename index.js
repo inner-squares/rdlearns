a35f71f17a6f5ea6fdadaa661991385eca44491d
FiT Lab <rdrink@sartic.edu>
2015-11-23 17:09:18 -0600

    added columns to db output

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
-  //res.send('Hello World!')
-  Harvest.findOne({ where: {crop: 0} }).then(function(theCrop) {
+  // res.send('Hello World!')
+  Harvest.findOne({ where: {crop: 0} }).then(function (theCrop) {
     // project will be the first entry of the Projects table with the title 'aProject' || null
     res.send(theCrop)
   })
@@ -16,9 +16,8 @@ var server = app.listen(3000, function () {
   console.log('Example app listening at http://%s:%s', host, port)
 })
 
-
-var Sequelize = require('sequelize');
-//var sequelize = new Sequelize('farmland', 'joe_user', '1234');
+var Sequelize = require('sequelize')
+// var sequelize = new Sequelize('farmland', 'joe_user', '1234');
 var sequelize = new Sequelize('farmland', 'root', 'raspberry', {
   host: 'localhost',
   dialect: 'mysql',
@@ -27,17 +26,32 @@ var sequelize = new Sequelize('farmland', 'root', 'raspberry', {
     max: 5,
     min: 0,
     idle: 10000
-  },
+  }
 
-});
+})
 
 var Harvest = sequelize.define('harvest', {
   crop: {
     type: Sequelize.INTEGER,
     primaryKey: true
   },
+  examined: {
+    type: Sequelize.BIGINT
+  },
   seed: {
     type: Sequelize.BIGINT
+  },
+  fertilizer: {
+    type: Sequelize.STRING
+  },
+  solution: {
+    type: Sequelize.STRING
+  },
+  harvest: {
+    type: Sequelize.STRING
+  },
+  solver: {
+    type: Sequelize.STRING
   }
 }, {
   freezeTableName: false, // Model tableName will not be the same as the model name
@@ -45,15 +59,15 @@ var Harvest = sequelize.define('harvest', {
   // define the table's name
   tableName: 'upper_forty',
   timestamps: false
-});
+})
 
 // search for attributes
-Harvest.findOne({ where: {crop: 0} }).then(function(theCrop) {
+Harvest.findOne({ where: {crop: 0} }).then(function (theCrop) {
   // project will be the first entry of the Projects table with the title 'aProject' || null
   console.log(theCrop)
 })
 
-/*User.sync({force: true}).then(function () {
+/* User.sync({force: true}).then(function () {
   // Table created
   return User.create({
     firstName: 'John',
