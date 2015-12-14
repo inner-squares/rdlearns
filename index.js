b6e8217b9ce3c15e45ffe3ed7f3c442b469f276c
Andrew Monks <a@monks.co>
2015-12-14 17:54:18 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 
-var harvest_finder = require('harvest_finder')
+var harvest_finder = require('./harvest_finder')
 
 // app.get('/', function (req, res) {
 //   // res.send('Hello World!')
