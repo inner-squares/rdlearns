5c08582d6bef9d84e87ad8df69368d930e1d867c
FiT Lab <rdrink@sartic.edu>
2016-02-05 12:57:53 -0600

    add log

--- fertilizer_index.js
+++ fertilizer_index.js
@@ -1,9 +1,11 @@
 var Harvest = require('./harvest_model')
 
-module.exports = function (req, res) {
+module.exports = function (req, res) // requst & response
+{
   Harvest.aggregate('fertilizer', 'DISTINCT', {
     plain: false
   }).then(function (theCrop) {
+    console.log(theCrop)
     res.send(theCrop)
   })
 }
